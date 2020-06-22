5f79efb74f53ee99c9a3aa9da0fcee8202103734
Felipe <tejeria.felipe@gmail.com>
2020-06-22 16:38:50 -0300

    se genera el archivo.huff comprimido

--- huff.py
+++ huff.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
 import argparse
+import collections
 from collections import Counter
 
 from heapq import heappush, heappop, heapify
 from collections import defaultdict
 
 def main():
+    archivo ='prueba'
     parser = argparse.ArgumentParser(description='Comprimir archivos usando un árbol de Huffman ')  # generamos objeto parser y le damos una descripcion
     parser.add_argument('-a', '--archivo',help ='archivo a comprimir ',  required=False, type=argparse.FileType('r')) #por mientras queda false para poder correr
     parser.add_argument('-f', '--froce', help='forzar la compresión, aunque el archivo resultante sea más grande', required=False, type=str)
     parser.add_argument('-v', '--verbose', help='escribe en stderr información sobre el avance del proceso,por ejemplo, los bitcodes para cada símbolo', required=False, type=str)
 
     args = parser.parse_args()
-
-    txt = "hola como estas"
+    file = open("archivo.huff", "w")
+    txt = args.archivo.read()
     symb2freq = defaultdict(int)
-    for ch in txt:
-        symb2freq[ch] += 1
-    # in Python 3.1+:
-    # symb2freq = collections.Counter(txt)
+    symb2freq = collections.Counter(txt)
     huff = encode(symb2freq)
     print("Symbol\tWeight\tHuffman Code")
     for p in huff:
         print( "%s\t%s\t%s" % (p[0], symb2freq[p[0]], p[1]))
+        file.write(p[1])
+
+    file.close()
 
 
 
