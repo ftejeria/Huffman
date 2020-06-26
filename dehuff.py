b5dcb148279fe376fdd8a507d3679bd10042e429
Germán <gabuarab@correo.um.edu.uy>
2020-06-26 17:57:33 -0300

    Se agregan Excepciones y detalles del quinke

--- dehuff.py
+++ dehuff.py
@@ -5,72 +5,95 @@ import struct
 
 MAGIC_NUMBER = 69
 
+
+
+
 def main():
     parser = argparse.ArgumentParser(description='Descomprimir archivos usando un árbol de Huffman ')  # generamos objeto parser y le damos una descripcion
     parser.add_argument('-v', '--verbose', help='escribe en stderr información sobre el avance del proceso,por ejemplo, los bitcodes para cada símbolo', required=False,action="store_true")
     parser.add_argument('archivo', help='archivo a descomprimir ',
                         type=argparse.FileType('rb'))
     args = parser.parse_args()
-    nombreArchivo = args.archivo.name
-    vecNombre=nombreArchivo.split('.')
-    if (vecNombre[-1] != "huf"):
-        print("El archivo no es del formato '.huf' , no se puede descomprimir")
-        return 0
-    txt=args.archivo.read()
+
+
+    try:
+       txt,vecNombre=verificarArchivo(args)
+    except:
+        return -1 #dehuff devuelve -1 y corta el proceso
+
+
 
     parteNombre=''
     for i in range(len(vecNombre)-1):
         parteNombre+=vecNombre[i]
     file=open(parteNombre+".ori","w")
 
-    #print(txt[:2])  # numero magico
-    #print(txt[2:3])  # cantidad de simbolos que hay
-    #print(txt[3:4])  # tamañano de cada simbolo (6)
-    #print(txt[4:8])  # largo del archivo original
 
     cabezal = struct.unpack('!hBBi', txt[:8])
+
+
+
     dic_codificacion={}
-
-    if args.verbose:
-        print("Caracter / Codigo")
-    for i in range(8,8+cabezal[1]*cabezal[2],cabezal[2]):
-        tuplaCodigo=struct.unpack('!BBi',txt[i:i+cabezal[2]])
-        numeroBin='{0:b}'.format(tuplaCodigo[2]) # escribe en binario de la forma mas corta
-        parteDeCeros=''
-        for _ in range(tuplaCodigo[1]-len(numeroBin)):
-            parteDeCeros+='0'
-        clave=str(parteDeCeros+numeroBin)
-        if args.verbose:
-            print(f'"{chr(tuplaCodigo[0])}"  /  {clave}')
-        dic_codificacion[clave]=chr(tuplaCodigo[0])
-
+    generarDiccionario(args,txt,dic_codificacion,cabezal)
 
     textobinario=''
+    textobinario=generarTextoEnBinario(textobinario,cabezal,txt)
 
-    for j in range(8+cabezal[1]*cabezal[2],len(txt)):#leo de a bite y lo guardo como 0 y 1 en un string
-        parteDeCeros = ''
-        binario=bin(txt[j])[2:] #txt j ya da el numerno en int
-        for _ in range(8-len(binario)):
-            parteDeCeros+='0'
-        binario = parteDeCeros + binario
-        textobinario+=binario
-
-    bitsacumulado = 0
-    
-    for num in range(len(textobinario)):
-        if textobinario[num-bitsacumulado:num+1] not in dic_codificacion:
-            bitsacumulado+=1
-        else:
-            file.write(dic_codificacion[textobinario[num-bitsacumulado:num+1]])
-            bitsacumulado=0
-
-
+    decodificarTexto(textobinario,dic_codificacion,file)
     file.close()
-    
+    print("Archivo descomprimido exitosamente")
+    return 0
+
+def verificarArchivo(args):
+    '''Verifica que el archivo sea un archivo.huf'''
+    nombreArchivo = args.archivo.name
+    vecNombre = nombreArchivo.split('.')
+    if (vecNombre[-1] != "huf"):
+        print("El archivo no es del formato '.huf' , no se puede descomprimir")
+        raise Exception
+    return args.archivo.read(),vecNombre
+
+def generarDiccionario(args,txt,dic_codificacion,cabezal):
+    '''Esta funcion sirve para generar un diccionario que contega el codigo en binario de los caracteres del archivo como key y el valor sera
+    ser el caracter en si.Se le pasa demas args para usar el args.versbose y asi imprimir el simbolo y el codigo a la vez que se crea el diccionario,
+    de esta manera se hace eficientemente.'''
+    if args.verbose:
+        print("Caracter / Codigo")
+    for i in range(8, 8 + cabezal[1] * cabezal[2], cabezal[2]):
+        tuplaCodigo = struct.unpack('!BBi', txt[i:i + cabezal[2]])
+        numeroBin = '{0:b}'.format(tuplaCodigo[2])  # escribe en binario de la forma mas corta
+        parteDeCeros = ''
+        for _ in range(tuplaCodigo[1] - len(numeroBin)):
+            parteDeCeros += '0'
+        clave = str(parteDeCeros + numeroBin)
+        if args.verbose:
+            print(f'"{chr(tuplaCodigo[0])}"  /  {clave}')
+        dic_codificacion[clave] = chr(tuplaCodigo[0])
 
 
 
-     #decodificar y copiar el texto que queda (lo da el cabezal)
+def generarTextoEnBinario(textobinario, cabezal, txt):
+    '''Se pasa el texto del archivo original de bites a binario '''
+    for j in range(8 + cabezal[1] * cabezal[2], len(txt)):  # leo de a bite y lo guardo como 0 y 1 en un string
+        parteDeCeros = ''
+        binario = bin(txt[j])[2:]  # txt j ya da el numerno en int
+        for _ in range(8 - len(binario)):
+            parteDeCeros += '0'
+        binario = parteDeCeros + binario
+        textobinario += binario
+    return textobinario
+
+
+def decodificarTexto(textobinario, dic_codificacion,file):
+    '''Se decodifica el texto binario usando el diccionario y a su vez se va escribiendo en el archivo.ori'''
+    bitsacumulado = 0
+    for num in range(len(textobinario)):
+        if textobinario[num - bitsacumulado:num + 1] not in dic_codificacion:
+            bitsacumulado += 1
+        else:
+            file.write(dic_codificacion[textobinario[num - bitsacumulado:num + 1]])
+            bitsacumulado = 0
+
 
 
 
--- huff.py
+++ huff.py
@@ -12,6 +12,18 @@ MAGIC_NUMBER = 69 # magic number for identification
 SIZE_ARR = 6
 
 
+def forceCierre(txt, contador, args, file):
+    if (len(txt) < contador) and not args.force:
+        file.close()
+        print("El archivo comprimido es mas grande que el original no se comprimio")
+        os.remove(args.archivo.name + ".huf")
+        raise Exception
+    else:
+        file.close()
+        print("Archivo comprimido exitosamente")
+
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Comprimir archivos usando un árbol de Huffman ')  # generamos objeto parser y le damos una descripcion
@@ -26,58 +38,46 @@ def main():
     args = parser.parse_args()
     file = open(args.archivo.name +".huf", "wb+")
     txt = args.archivo.read()
+
     symb2freq = collections.Counter(txt)
     huff = encode(symb2freq)
-    dicc_symbols={p[0]:p[1] for p in huff } # Estructura que nos va a ayudar para transcribir el texto
-    list_symbols=[(p[0],p[1]) for p in huff] #lista de Caracteres y sus codigos
-    if args.verbose:
-        print("Caracter / Codigo")
-        for tupla in list_symbols:
-            print(f'{tupla[0]}   /  {tupla[1]}')
-    cabezal = struct.pack('!hBBi',MAGIC_NUMBER,len(list_symbols),SIZE_ARR,len(txt)) #TODO ARREGLAR EL EMA DE QUE USA MENOS BITES
+
+    dicc_symbols,list_symbols=generarDicc(huff,args)
+
+    #Generar el Cabezal del archivo.huf
+    cabezal = struct.pack('!hBBi',MAGIC_NUMBER,len(list_symbols),SIZE_ARR,len(txt))
+
+    #Contador nos va a servir para comparar el tamaño del archivo original y el nuevo
     contador=len(cabezal)
     file.write(cabezal)
 
-    encoded_table = []
-    for p in huff:
-        symbol = struct.pack('!B', int(format(ord(p[0]), 'd')))
-        code_len = struct.pack('!B', len(p[1]))
-        code = struct.pack('!i', int(p[1], 2))
-        encoded_table.append([symbol, code_len, code])
 
+    encoded_table = generarTablaDeCodigo(huff)
+
+    #Escribir la codificacion en el archivo
     for c in encoded_table:
         for s in c:
            contador = contador + len(s)
            file.write(s)
 
-
+    #Esccribimos los caracteres del texto con sus codigos en binario
     texto_codificado = ''
-    bites=[]
     for char in txt:
         texto_codificado+=dicc_symbols[char]
 
-
-    for i in range(0,len(texto_codificado),8):
-        byte=texto_codificado[i:i+8]
-        if len(byte)==8:
-            bites.append(int(byte,2))
-        else:
-            byte += (8-len(byte))*'0'
-            bites.append(int(byte,2))
+    bites=comprimirTextoCodificado(texto_codificado)
+    contador = contador + len(bites)
 
 
-    contador=contador+len(bites)
+    #Paso el texto a bytes
     texto_codificado=bytes(bites)
     file.write(texto_codificado)
 
+    try:
+        forceCierre(txt,contador,args,file)
+    except:
+        return -1  # dehuff devuelve -1 y corta el proceso
 
-    if (len(txt) < contador) and not args.force:
-         file.close()
-         print("El archivo comprimido es mas grande que el original no se Comprimio")
-         os.remove(args.archivo.name +".huf")
-    else:
-        file.close()
-        print("Archivo comprimido exitosamente")
     return 0
 
 
@@ -94,6 +94,43 @@ def encode(symb2freq):
             pair[1] = '1' + pair[1]
         heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
     return sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+
+
+def generarDicc(huff, args):
+    '''Genera un diccionario y una lista con los simbolos y su codigo.Ademas se le pasa args  por si se usa verbose para que imprima
+    la codificacion en la terminal'''
+
+    dicc_symbols = {p[0]: p[1] for p in huff}  # Estructura que nos va a ayudar para transcribir el texto
+    list_symbols = [(p[0], p[1]) for p in huff]  # lista de Caracteres y sus codigos
+    if args.verbose:
+        print("Caracter / Codigo")
+        for tupla in list_symbols:
+            print(f'{tupla[0]}   /  {tupla[1]}')
+    return dicc_symbols, list_symbols
+
+
+def generarTablaDeCodigo(huff):
+    '''Genera una tabla con los simbolos , el largo del su codigo y su codigo'''
+    encoded_table = []
+    for p in huff:
+        symbol = struct.pack('!B', int(format(ord(p[0]), 'd')))
+        code_len = struct.pack('!B', len(p[1]))
+        code = struct.pack('!i', int(p[1], 2))
+        encoded_table.append([symbol, code_len, code])
+    return encoded_table
+
+def comprimirTextoCodificado(texto_codificado):
+    '''Agarro el chorizo de 0 y 1  y los separa de a 8 bites, y lo guarda en un vector, si llega a faltar le agrega 0 al final '''
+    bites = []
+    for i in range(0,len(texto_codificado),8):
+        byte=texto_codificado[i:i+8]
+        if len(byte)==8:
+            bites.append(int(byte,2))
+        else:
+            byte += (8-len(byte))*'0'
+            bites.append(int(byte,2))
+        return bites
+
 
 if __name__ == '__main__':
     main()
